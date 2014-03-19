b184f3554f669cc9456f757cc2139b0328471566
Paul Smith <p@ulsmith.net>
2014-03-19 22:10:38 +0000

    New extension db file ofr use with extension system

---
+++ storage/database/extension.db.php
@@ -0,0 +1,10 @@
+<?php
+// lock:0
+// inc:1-----------------------------
+// row_count:0-----------------------------
+// --- start headers ---
+// table:extension
+// columns:1:id:int:0|2:extension:string:0|3:type:string:0|4:handle:string:0|5:json_settings:string:1|6:user_id:int:0|7:access_level:int:0
+// ---
+// ---
+// --- end headers ---
