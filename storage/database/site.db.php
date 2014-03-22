cc7d9d774f0cb0aa25fd1515fcd876927e1aaaa7
Paul Smith <p@ulsmith.net>
2014-03-22 11:49:35 +0000

    Added version numbering to site db

--- storage/database/site.db.php
+++ storage/database/site.db.php
@@ -4,8 +4,8 @@
 // row_count:1-----------------------------
 // --- start headers ---
 // table:site
-// columns:1:id:int:0|2:name:string:0|3:home_page:int:1
+// columns:1:id:int:0|2:name:string:0|3:home_page:int:1|4:version:int:0|5:milestone:int:0|4:release:int:0
 // ---
 // ---
 // --- end headers ---
-// row:1`1`|2`My Site`|3`1`
+// row:1`1`|2`My Site`|3`1`|4`3`|5`1`|6`0`
