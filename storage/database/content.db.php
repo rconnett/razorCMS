f4bf3993c33e222254556c0dc31c860ea9890070
Paul Smith <p@ulsmith.net>
2014-03-24 08:04:46 +0000

    Default content added

--- storage/database/content.db.php
+++ storage/database/content.db.php
@@ -1,7 +1,7 @@
 <?php
 // lock:0
-// inc:2-----------------------------
-// row_count:1-----------------------------
+// inc:4-----------------------------
+// row_count:3-----------------------------
 // --- start headers ---
 // table:content
 // columns:1:id:int:0|2:name:string:1|3:content:string:0
@@ -9,3 +9,5 @@
 // ---
 // --- end headers ---
 // row:1`1`|2`header intro`|3`<h1>razorCMS V3</h1><p>File Based Content Management System (FBCMS)</p>`
+// row:1`2`|2`Main content - In the beginning`|3`<h3><i class="fa fa-leaf"></i> In The Beginning...</h3><p>razorCMS began as a databaseless flat file content management system, forked from a project called uCMS. It's structure allowed you to have just the amount of functionality you needed in a flat file CMS solution, adding extensions (blade packs) for further functionality, whilst allowing setup on simple servers with no database.</p><p>Starting with a core system install, razorCMS gave you base functionality required to run a website, add the extra functionality as you needed it via the blade pack management system. Everything from WYSIWYG editors to SEF URL was added as extra functionality.</p>`
+// row:1`3`|2`Main content - the future`|3`<h3><i class="fa fa-rocket"></i> Looking Forward...</h3><p>Since its conception, technology has changed dramatically, with more and more functionality being handled via JavaScript. In addition to this, a more modular object based approached can be used to make things more extendible, allowing for integration with other technologies like mobile applications.</p><p>With all this in mind, razorCMS V3 was born, utilising technologies like angularJS, PHP and RESTfull approaches to dealing with data management. Yes we are still a file based CMS, but we are no longer flat file, adapting a new method for file based DB storage.</p>`
