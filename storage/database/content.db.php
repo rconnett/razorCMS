fc1f56b0f8d0ea0b91a97d05845a631cfe2ce565
Paul Smith <p@ulsmith.net>
2014-03-24 07:43:41 +0000

    added sample content for home page.

--- storage/database/content.db.php
+++ storage/database/content.db.php
@@ -1,10 +1,11 @@
 <?php
 // lock:0
-// inc:1-----------------------------
-// row_count:0-----------------------------
+// inc:2-----------------------------
+// row_count:1-----------------------------
 // --- start headers ---
 // table:content
 // columns:1:id:int:0|2:name:string:1|3:content:string:0
 // ---
 // ---
 // --- end headers ---
+// row:1`1`|2`header intro`|3`<h1>razorCMS V3</h1><p>File Based Content Management System (FBCMS)</p>`
