e682e53f502b18619a9a58763d38663941743c44
Paul Smith <p@ulsmith.net>
2014-03-26 22:18:17 +0000

    moved site version

--- storage/database/system.db.php
+++ storage/database/system.db.php
@@ -4,8 +4,8 @@
 // row_count:1-----------------------------
 // --- start headers ---
 // table:site
-// columns:1:id:int:0|2:name:string:0|3:home_page:int:1
+// columns:1:id:int:0|2:version:int:0|3:milestone:int:0|4:release:int:0
 // ---
 // ---
 // --- end headers ---
-// row:1`1`|2`My Site`|3`1`
+// row:1`1`|2`3`|3`1`|4`2`
