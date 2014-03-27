9c60b8de01ff725f8e7e3386d35c3102e7211cd3
Paul Smith <p@ulsmith.net>
2014-03-27 21:45:55 +0000

    Updated page content db file for future settings per content allocation

--- storage/database/page_content.db.php
+++ storage/database/page_content.db.php
@@ -4,13 +4,13 @@
 // row_count:6-----------------------------
 // --- start headers ---
 // table:page_content
-// columns:1:id:int:0|2:page_id:int:0|3:content_id:int:1|4:extension:string:1|5:location:string:0|6:column:int:0|7:position:int:0
+// columns:1:id:int:0|2:page_id:int:0|3:content_id:int:1|4:extension:string:1|5:location:string:0|6:column:int:0|7:position:int:0|8:json_settings:string:1
 // ---
 // ---
 // --- end headers ---
-// row:1`1`|2`1`|3`1`|4``|5`header`|6`1`|7`1`
-// row:1`2`|2`1`|3`2`|4``|5`main`|6`1`|7`1`
-// row:1`3`|2`1`|3`3`|4``|5`main`|6`1`|7`2`
-// row:1`5`|2`2`|3`1`|4``|5`header`|6`1`|7`1`
-// row:1`6`|2`2`|3`4`|4``|5`main`|6`1`|7`1`
-// row:1`7`|2`2`|3`5`|4``|5`main`|6`1`|7`2`
+// row:1`1`|2`1`|3`1`|4``|5`header`|6`1`|7`1`|null
+// row:1`2`|2`1`|3`2`|4``|5`main`|6`1`|7`1`|null
+// row:1`3`|2`1`|3`3`|4``|5`main`|6`1`|7`2`|null
+// row:1`5`|2`2`|3`1`|4``|5`header`|6`1`|7`1`|null
+// row:1`6`|2`2`|3`4`|4``|5`main`|6`1`|7`1`|null
+// row:1`7`|2`2`|3`5`|4``|5`main`|6`1`|7`2`|null
