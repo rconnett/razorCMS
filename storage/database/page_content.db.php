524170febf05eb0c3133a86311962f7b2e7c9a06
Paul Smith <p@ulsmith.net>
2014-03-28 19:10:31 +0000

    Removed content to return to default content set

--- storage/database/page_content.db.php
+++ storage/database/page_content.db.php
@@ -1,7 +1,7 @@
 <?php
 // lock:0
 // inc:14----------------------------
-// row_count:7-----------------------------
+// row_count:6-----------------------------
 // --- start headers ---
 // table:page_content
 // columns:1:id:int:0|2:page_id:int:0|3:content_id:int:1|4:extension:string:1|5:location:string:0|6:column:int:0|7:position:int:0|8:json_settings:string:1
@@ -14,4 +14,3 @@
 // row:1`5`|2`2`|3`1`|4``|5`header`|6`1`|7`1`|8``
 // row:1`6`|2`2`|3`4`|4``|5`main`|6`1`|7`1`|8``
 // row:1`7`|2`2`|3`5`|4``|5`main`|6`1`|7`2`|8``
-// row:1`13`|2`2`|3`0`|4`communication/razorcms/contact-form/contact-form.manifest.json`|5`main`|6`1`|7`3`|8``
