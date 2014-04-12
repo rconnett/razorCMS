54324f193983149ca1ed96800008a4a9918e4745
Paul Smith <p@ulsmith.net>
2014-04-12 21:49:31 +0100

    further tweaks to photo extension, ready for release

--- extension/photo/razorcms/photo-gallery/js/controller.js
+++ extension/photo/razorcms/photo-gallery/js/controller.js
@@ -2,12 +2,14 @@ define(["angular"], function(angular)
 {
     angular.module("extension.photo.razorcms.photoGallery.controller", [])
 
-    .controller("photoGallery", function($scope)
+    .controller("photoGallery", function($scope, $timeout)
     {
         $scope.photos = [];
         $scope.photoFrame = null;
+        $scope.photoFrameHelper = null;
         $scope.position = 0;
         $scope.sliderListStyle = {"width": "0px", "margin-left": "10px"};
+        $scope.helperCanvasStyle = {"z-index": "-1px"};
 
         $scope.init = function(photos)
         {
@@ -39,15 +41,40 @@ define(["angular"], function(angular)
 
         $scope.scrollPhotos = function(direction)
         {
-            if (direction == "left" && $scope.position > 0) $scope.position--;
-            else if (direction == "right" && $scope.position < $scope.photos.length - 1) $scope.position++;
-            $scope.photoFrame = $scope.photos[$scope.position];         
+            if (direction == "left" && $scope.position > 0)
+            {
+                $scope.position--;
+                $scope.changePhoto();
+            }
+            else if (direction == "right" && $scope.position < $scope.photos.length - 1)
+            {
+                $scope.position++;
+                $scope.changePhoto();
+            }
         };
 
         $scope.selectPhoto = function(index)
         {
             $scope.position = index;
-            $scope.photoFrame = $scope.photos[$scope.position];
+            $scope.changePhoto();
+        };
+
+        $scope.changePhoto = function()
+        {
+            $scope.showBox = false;
+            $scope.turnPhoto = true;
+            
+            // change helper and reset
+            $timeout(function() 
+            {
+                $scope.photoFrame = $scope.photos[$scope.position];
+                    $timeout(function()
+                    {
+                        $scope.turnPhoto = false;
+                        $scope.showBox = true;
+                    }, 300);
+            }, 300);
+            
         };
     });
 });
--- extension/photo/razorcms/photo-gallery/view/photo-gallery.php
+++ extension/photo/razorcms/photo-gallery/view/photo-gallery.php
@@ -1,7 +1,7 @@
 <?php if (!defined("RAZOR_BASE_PATH")) die("No direct script access to this content"); ?>
 
 <?php
-	$image_files = array(".jpg", ".jpeg", ".png", ".gif");
+	$image_files = array(".jpg", "jpeg", ".png", ".gif");
 
 	// grab settings for this content area and from that, find folder to use
 	$content_ext_settings = json_decode($c_data["json_settings"]);
@@ -26,7 +26,14 @@
 				continue;
 			}
 
-			$files[$key] = RAZOR_BASE_URL."storage/files/razorcms/photo-gallery/{$content_ext_settings->album_name}/{$file}";
+			$files[$key] = array("url" => RAZOR_BASE_URL."storage/files/razorcms/photo-gallery/{$content_ext_settings->album_name}/{$file}");
+
+			if (strtolower(substr($file, -4) == ".jpg" || strtolower(substr($file, -4) == "jpeg")))
+			{
+				$details = exif_read_data(RAZOR_BASE_PATH."storage/files/razorcms/photo-gallery/{$content_ext_settings->album_name}/{$file}");
+				if (isset($details["DocumentName"])) $files[$key]["title"] = $details["DocumentName"];
+				if (isset($details["ImageDescription"])) $files[$key]["description"] = $details["ImageDescription"];
+			}
 		}
 
 		sort($files);
@@ -50,20 +57,26 @@
 <div class="photo-razorcms-photo-gallery" class="ng-cloak" ng-controller="photoGallery" ng-init="init(<?php echo $photos ?>)">
 	<div class="photo-gallery-frame text-center" style="height: <?php echo $m["frame_height"] ?>; width: <?php echo $m["frame_width"] ?>;">
 		<div class="photo-gallery-canvas">
-			<i class="fa fa-chevron-circle-left photo-control change-left" ng-click="scrollPhotos('left')"></i>
-			<i class="fa fa-chevron-circle-right photo-control change-right" ng-click="scrollPhotos('right')"></i>
+			<div class="photo-details ng-cloak" ng-if="photoFrame.title || photoFrame.description">
+				<div class="details-box" ng-class="{'show-box': showBox}">
+					<p class="text-center"><strong>{{photoFrame.title}}</strong></p>
+					<p class="text-center">{{photoFrame.description}}</p>
+				</div>
+			</div>
+			<i class="fa fa-chevron-circle-left photo-control change-left" ng-click="scrollPhotos('left')" ng-hide="position == 0"></i>
+			<i class="fa fa-chevron-circle-right photo-control change-right" ng-click="scrollPhotos('right')" ng-hide="position == photos.length - 1"></i>
 			<div class="center-box" style="line-height: <?php echo $m["frame_height"] ?>;">
-				<img ng-show="photoFrame" ng-src="{{photoFrame}}" ng-init="photoFrame = photos[position]">
+				<img ng-show="photoFrame" ng-src="{{photoFrame.url}}" ng-init="photoFrame = photos[position]" ng-class="{'turn-photo': turnPhoto}">
 				<i ng-if="!photoFrame" class="fa fa-picture-o photo-placeholder"></i>
 			</div>
-		</div>
+		</div>		
 	</div>
 	<div class="photo-gallery-controls" style="width: <?php echo $m["frame_width"] ?>;">
 		<i class="fa fa-chevron-circle-left photo-control slide-left" ng-click="scrollThumbs('left')"></i>
 		<i class="fa fa-chevron-circle-right photo-control slide-right" ng-click="scrollThumbs('right')"></i>
 		<div class="photo-gallery-slider">
 			<ul class="photo-gallery-thumbs" ng-style="sliderListStyle">
-				<li ng-repeat="p in photos"><img ng-src="{{p}}" ng-click="selectPhoto($index)" ng-class="{'selected': $index == position}"></li>
+				<li ng-repeat="p in photos"><img ng-src="{{p.url}}" ng-click="selectPhoto($index)" ng-class="{'selected': $index == position}"></li>
 			</ul>
 		</div>
 	</div>
