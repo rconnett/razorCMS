976196ef5022cab4ba6cb43d482e1af36d79e825
Jesse Gangi <jgangi@aerialapps.com>
2014-05-23 17:23:24 -0500

    Fixed indenting issues.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+
 
 /**
  * razorCMS FBCMS
@@ -9,35 +10,37 @@
  * @site ulsmith.net
  * @created Feb 2014
  */
- 
-    // set session
-    session_start();
-    session_regenerate_id();
-	
-    // sidewide constants
-    define("RAZOR_BASE_PATH", str_replace(array("index.php"), "", $_SERVER["SCRIPT_FILENAME"]));
-    $port = ($_SERVER["SERVER_PORT"] == "80" ? "" : ":{$_SERVER["SERVER_PORT"]}");
-    define("RAZOR_BASE_URL", "http://".$_SERVER["SERVER_NAME"].$port.str_replace(array("index.php"), "", $_SERVER["SCRIPT_NAME"]));
-    define("RAZOR_USERS_IP", $_SERVER["REMOTE_ADDR"]);
-    define("RAZOR_USERS_UAGENT", $_SERVER["HTTP_USER_AGENT"]);
 
-    // includes
-	include_once(RAZOR_BASE_PATH.'library/php/razor/razor_file_tools.php');
-	include_once(RAZOR_BASE_PATH.'library/php/razor/razor_error_handler.php');
-    include_once(RAZOR_BASE_PATH.'library/php/razor/razor_site.php');
-    include_once(RAZOR_BASE_PATH."library/php/razor/razor_db.php");
 
-	// Load error handler
-	$error = new RazorErrorHandler();
-	set_error_handler(array($error, 'handle_error'));
-	set_exception_handler(array($error, 'handle_error'));
+// set session
+session_start();
+session_regenerate_id();
 
-    // check any required folders exist
-    if (!is_dir(RAZOR_BASE_PATH."extension")) mkdir(RAZOR_BASE_PATH."extension");
+// sidewide constants
+define("RAZOR_BASE_PATH", str_replace(array("index.php"), "", $_SERVER["SCRIPT_FILENAME"]));
+$port = ($_SERVER["SERVER_PORT"] == "80" ? "" : ":{$_SERVER["SERVER_PORT"]}");
+define("RAZOR_BASE_URL", "http://".$_SERVER["SERVER_NAME"].$port.str_replace(array("index.php"), "", $_SERVER["SCRIPT_NAME"]));
+define("RAZOR_USERS_IP", $_SERVER["REMOTE_ADDR"]);
+define("RAZOR_USERS_UAGENT", $_SERVER["HTTP_USER_AGENT"]);
 
-    // continue with public load
-    $site = new RazorSite();
-    $site->load();
-    $site->render();
+// includes
+include_once(RAZOR_BASE_PATH.'library/php/razor/razor_file_tools.php');
+include_once(RAZOR_BASE_PATH.'library/php/razor/razor_error_handler.php');
+include_once(RAZOR_BASE_PATH.'library/php/razor/razor_site.php');
+include_once(RAZOR_BASE_PATH."library/php/razor/razor_db.php");
+
+// Load error handler
+$error = new RazorErrorHandler();
+set_error_handler(array($error, 'handle_error'));
+set_exception_handler(array($error, 'handle_error'));
+
+// check any required folders exist
+if (!is_dir(RAZOR_BASE_PATH."extension")) mkdir(RAZOR_BASE_PATH."extension");
+
+// continue with public load
+$site = new RazorSite();
+$site->load();
+$site->render();
+
  
 /* PHP END */
