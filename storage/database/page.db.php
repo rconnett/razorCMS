6ee38c0ca5b8afd5454b02489183c04b0a51e8fa
Paul Smith <p@ulsmith.net>
2014-06-02 19:36:08 +0100

    updated database files

--- storage/database/page.db.php
+++ storage/database/page.db.php
@@ -1,7 +1,7 @@
 <?php
 // lock:0
-// inc:4-----------------------------
-// row_count:3-----------------------------
+// inc:3-----------------------------
+// row_count:2-----------------------------
 // --- start headers ---
 // table:page
 // columns:1:id:int:0|2:active:bool:0|3:theme:string:1|4:name:string:0|5:title:string:0|6:link:string:0|7:keywords:string:1|8:description:string:1|9:access_level:int:1|10:json_settings:string:1
@@ -10,4 +10,3 @@
 // --- end headers ---
 // row:1`1`|2`1`|3``|4`Home`|5`razorCMS Home Page`|6``|7`razorcms,cms,home,page`|8`Home page for razorCMS version 3`|9``|10``
 // row:1`2`|2`1`|3``|4`Test Content`|5`Test Content`|6`test-content.html`|7`test,content,page`|8`Test content page with some stuff on it.`|9`0`|10``
-// row:1`3`|2`0`|null|4`test`|5`test`|6`test.html`|7`fds`|8`fsd`|null|null
