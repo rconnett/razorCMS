c2c3cf8163ee0272475e6dfdc8a7dbbaa5bc5b1c
Paul Smith <p@ulsmith.net>
2015-02-15 09:46:24 +0000

    Removed as this was to get around version bug and forcing upgrade on 3.4.5, not needed after this version. (reverted from commit f717c391a3eda92e6003607cc71ff567e5de8846)

--- library/js/razor/admin/admin-access.js
+++ library/js/razor/admin/admin-access.js
@@ -93,6 +93,9 @@ define(["angular", "cookie-monster", "ui-bootstrap"], function(angular, monster)
 					var latestBuild = (data.version * 1000000) + (data.milestone * 1000) + data.release;
 					var systemBuild = (parseInt($scope.system.version) * 1000000) + (parseInt($scope.system.milestone) * 1000) + parseInt($scope.system.release);
 
+					// REMOVE AFTER THIS 3.4.5 release to fix bug with version numbers
+					if (latestBuild = 300004005) latestBuild = 3004005;
+
 					if (latestBuild > systemBuild) $scope.upgrade = true;
 					else $scope.noUpgrade = true;
 
